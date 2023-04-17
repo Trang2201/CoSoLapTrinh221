c8194ab2cae2cd044e33cec3cf09e88c744911b5
Zhuang <trang2212004@gmail.com>
2023-04-17 07:50:34 +0700

    .

--- Chuong3/BaiTapCuoiChuongP1/Bai3_5.py
+++ Chuong3/BaiTapCuoiChuongP1/Bai3_5.py
@@ -4,9 +4,9 @@ elif Songaynghi<2: print("Xep loai B")
 elif Songaynghi<4:  print("Xep loai C")
 else: print("Xep loai D")
 # cách hai:
-a=int(input())  # a là số ngày nghỉ
-if a==0: s='A'
-elif a<2: s='B'
-elif a<4: s='C'
-else: s='D'
-print("Xep loai",s)
+# a=int(input())  # a là số ngày nghỉ
+# if a==0: s='A'
+# elif a<2: s='B'
+# elif a<4: s='C'
+# else: s='D'
+# print("Xep loai",s)
--- Chuong3/BaiTapCuoiChuongP2/3.11.py
+++ Chuong3/BaiTapCuoiChuongP2/3.11.py
@@ -1,9 +1,9 @@
 d=0
 a=0
-n=int
-while n!=0:
+while True:
     n=int(input())
     if (n>0): d=d+1
-    if (n<0): a=a+1
+    elif (n<0): a=a+1
+    else: break
 print(d,"so duong")
 print(a,"so am")
